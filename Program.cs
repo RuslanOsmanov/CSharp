aa01604c6dc875c7efecfffc295a6409319e0775
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-10-27 23:10:39 +0300

    Add files via upload
    Пользователь вводит номер класса. Программа выводит к каким классам относиться этот, т.е к младшим, средним, старшим.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string clas;
+            switch (n)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    clas = "Младшие";
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    clas = "Средние";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    clas = "Старшие";
+                    break;
+                default:
+                    clas = "Ошибка: класса с номером " + n + " не существует";
+                    break;
+            }
+            Console.WriteLine(clas);
+            Console.ReadLine();
+        }
+    }
+}
