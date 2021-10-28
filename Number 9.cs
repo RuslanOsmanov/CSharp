c42ec78337d2dbb7359bb7bab349f8299632dbc5
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-10-28 15:40:46 +0300

    Add files via upload
    Вывести на экран, консольного приложения, прямоугольный треугольник из звездочек “*”, с длиной катета m.

---
+++ Number 9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину катета");
+            int m = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
