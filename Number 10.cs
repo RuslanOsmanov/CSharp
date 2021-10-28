a37da34569c7ae2e0d0a9f5e6223e3562e997377
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-10-28 16:29:21 +0300

    Add files via upload
    Используя вложенные циклы, вывести таблицы умножения от второй до шестой.

---
+++ Number 10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапозон таблицы умножения");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            while (n <= m)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} * {i} = {n * i}");
+                }
+                n++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
