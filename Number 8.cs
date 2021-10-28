ecfdc2cdb92bbf2fe30637b63fd308bd3b3838f2
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-10-28 15:21:18 +0300

    Add files via upload
    Посчитать количество и сумму парных чисел в диапазоне от 1 до N.

---
+++ Number 8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sumi = 0;
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    count += 1;
+                    sumi += i;
+                }
+            }
+            Console.WriteLine(count);
+            Console.WriteLine(sumi);
+        }
+    }
+}
