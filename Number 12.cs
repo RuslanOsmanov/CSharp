2005d31b9cab9dff9eb1ef6a15683cb33648001d
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-10-31 23:13:00 +0300

    Add files via upload
    В массиве действительных чисел все нулевые элементы заменить на среднее арифметическое всех элементов массива.

---
+++ Number 12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double [] array = new double [n];
+            double sumi = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i+1}-й элемент массива");
+                array[i] = double.Parse(Console.ReadLine());
+                sumi += array[i];
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == 0)
+                {
+                    array[i] = sumi / n;
+                }
+                Console.WriteLine($"{i+1} элемент массива равен: " + array[i].ToString());
+            }
+
+        }
+    }
+}
