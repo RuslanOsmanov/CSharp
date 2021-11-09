2a2aea1e4018803d4889e9aa0c6870844fe5b723
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-11-09 22:24:49 +0300

    Add files via upload
    Дан одномерный массив из 8 элементов. Заменить все элементы массива меньшие 15 их удвоенными значениями. Вывести на экран монитора преобразованный массив.

---
+++ Number 0_016.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1}-й элемент массива");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] < 15)
+                {
+                    array[i] = array[i] * 2;
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1} элемент массива: " + array[i].ToString());
+            }
+        }
+    }
+}
