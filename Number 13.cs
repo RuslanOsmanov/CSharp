2df44dcc1483ca5436c01df64ced5c74199b6edc
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-11-09 19:26:26 +0300

    Add files via upload
    Дан одномерный массив, состоящий из N целочисленных элементов. Ввести массив с клавиатуры. Найти минимальный элемент. Вывести индекс минимального элемента на экран.

---
+++ Number 13.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int [] array = new int [n];
+            int mini = 10000000;
+            int minic = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1}-й элемент массива");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] < mini)
+                {
+                    mini = array[i];
+                    minic = i;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Минимальный элемент: " + mini.ToString());
+            Console.WriteLine("Индекс минимального элемента: " + minic.ToString());
+        }
+    }
+}
