c4defd98dccd8ac428bb4f7de9f22fafd06f8d7d
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-12-08 15:54:58 +0300

    Add files via upload
    Дана строка, содержащая русскоязычный текст. Найти количество слов, начинающихся с буквы "о".

---
+++ Number 0_022.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        int counter = 0;
+
+        foreach (string word in rus.ToLower().Split(" "))
+        {
+            if (word.StartsWith("о"))
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine($"Количество слов, которые пишутся с \"о\": {counter}");
+    }
+}
