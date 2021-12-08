96782094c6773c10b408c19cdb2c4d12e33cafaa
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-12-08 16:01:01 +0300

    Add files via upload
    В строке заменить все двоеточия (:) знаком процента (%). Подсчитать количество замен.

---
+++ Number 0_023.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        int counter = 0;
+
+        for (int i = 0; i < rus.Length; i++)
+        {
+            if (rus[i] == ':')
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(rus.Replace(':','%'));
+        Console.WriteLine(counter);
+    }
+}
