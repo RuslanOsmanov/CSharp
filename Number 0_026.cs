5b790a40180c71b8c588f4df6433ddfccd90a4a0
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-12-08 16:16:57 +0300

    Add files via upload
    В строке заменить все заглавные буквы строчными.

---
+++ Number 0_026.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+
+{
+    //В строке заменить все заглавные буквы строчными.
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        Console.WriteLine(rus.ToLower());
+    }
+}
