662d9a8873ac7e15cd97664cae011a4858543d16
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-12-08 16:27:05 +0300

    Add files via upload
    Выполнить: Вводятся три числа — длины трех сторон треугольника. Создайте функцию Perimeter(), которая вычисляет периметр треугольника по длинам трех его сторон.
    Указание 1: Метод Perimeter() должен принимать в качестве аргументов три целых числа.
    Указание 2: Метод не должен возвращать никакого значения, поэтому вы должны использовать ключевое слово void в сигнатуре:
    static void Perimeter(...);
    Указание 3: Не забудьте преобразовать введенные значения в целые числа. Например:
    int a = int.Parse(Console.ReadLine());

---
+++ Number 0_028.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Perimeter(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+    }
+    static void Perimeter(int a, int b, int c)
+    {
+        int per = a + b + c;
+    }
+}
