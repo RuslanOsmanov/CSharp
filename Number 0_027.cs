4d85e213af8c6496cc8d0aa4c5832acd561781ea
Ruslan Osmanov <92995002+RuslanOsmanov@users.noreply.github.com>
2021-12-08 16:22:57 +0300

    Add files via upload
    Дана строка, заканчивающаяся точкой. Подсчитать, сколько слов в строке.

---
+++ Number 0_027.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+
+{
+    //Дана строка, заканчивающаяся точкой.
+    //Подсчитать, сколько слов в строке.
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        int count = 1;
+        if (rus.EndsWith('.'))
+        {
+            for (int i = 0; i < rus.Length; i++)
+            {
+                if (rus[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        else
+        {
+            Console.WriteLine("Строка не заканчивается на '.'");
+        }
+    }
+}
